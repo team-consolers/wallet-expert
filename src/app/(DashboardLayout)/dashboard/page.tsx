9646b4ad804a0e4561150bab5fbfe5f54a360e27
Aahmed-Hossain <aahmdctec@gmail.com>
2024-04-29 04:20:43 +0600

    balance summery widget completed

--- src/app/(DashboardLayout)/dashboard/page.tsx
+++ src/app/(DashboardLayout)/dashboard/page.tsx
@@ -5,10 +5,10 @@ import SummeryWidget from '@/components/summeryWidget/SummeryWidget';
 
 const DashboardPage = () => {
   return (
-    <div className="flex flex-wrap md:flex-wrap lg:flex-nowrap justify-between gap-3 md:gap-2 lg:gap-0 mt-3 md:mt-0">
+    <div className="flex flex-wrap md:flex-wrap lg:flex-nowrap justify-between gap-3 md:gap-2 lg:gap-2 mt-3 md:mt-0">
       {/* balance summary card */}
-      <div className="hover:shadow-2xl rounded-md border border-gray-300 px-4 py-8 w-full  ">
-       <h2 className="text-xl font-bold">Summary</h2>
+      <div className="hover:shadow-2xl rounded-md border border-gray-300 p-8 w-full lg:w-4/12">
+       <h2 className="text-xl font-bold mb-2">Summary</h2>
         <div className='flex justify-between'>
           <p className="font-semibold text-gray-600" >Balance</p> <p className='text-green-500'> ৳ 00.00</p>
         </div>
@@ -16,8 +16,8 @@ const DashboardPage = () => {
           <p className="font-semibold text-gray-600">Credit Cards: </p> <p className='text-red-500'> -৳ 00.00</p>
         </div>
         <Divider/>
-        <div className='flex justify-between'>
-          <p className="font-semibold text-gray-600">Total Remanings:</p> <p className='text-green-500'> -৳ 00.00</p>
+        <div className='flex justify-between mt-1'>
+          <p className="font-semibold text-gray-600 ">Total Remanings:</p> <p className='text-green-500'> -৳ 00.00</p>
         </div>
       </div>
       <SummeryWidget/>
--- src/components/summeryWidget/SummeryWidget.tsx
+++ src/components/summeryWidget/SummeryWidget.tsx
@@ -1,33 +1,32 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import { PieChart } from "@mui/x-charts/PieChart";
 import { useDrawingArea } from "@mui/x-charts/hooks";
 import { styled } from "@mui/material/styles";
 import { StyledEngineProvider } from "@mui/material/styles";
-import { Box, Divider } from '@mui/material';
+import { Box, Divider } from "@mui/material";
 const data = [
-    { value: 70, label: "Income", color: "#19A01E" },
-    { value: 30 , label: "Expenses", color: "#ED1C24" },
-  ];
-  const colors = ["#19A01E",  "#ED1C24"];
-  const size = {
-    width: 300,
-    height: 200,
-  };
-  const StyledText = styled("text")(({ theme }) => ({
-    fill: theme.palette.text.primary,
-    textAnchor: "middle",
-    font: "bold",
-    dominantBaseline: "central",
-    fontSize: 30,
-  }));
+  { value: 70, label: "Income", color: "#19A01E" },
+  { value: 30, label: "Expenses", color: "#ED1C24" },
+];
+const colors = ["#19A01E", "#ED1C24"];
+const size = {
+  width: 300,
+  height: 200,
+};
+const StyledText = styled("text")(({ theme }) => ({
+  fill: theme.palette.text.primary,
+  textAnchor: "middle",
+  font: "bold",
+  dominantBaseline: "central",
+  fontSize: 30,
+}));
 const SummeryWidget = () => {
-   
-      function PieCenterLabel({ children }:{children:React.ReactNode}) {
-        const { width, height, left, top } = useDrawingArea();
-        return (
-          <StyledText x={left + width / 2} y={top + height / 2}>
-            {/* <tspan fontSize="2rem" fontWeight="bold">
+  function PieCenterLabel({ children }: { children: React.ReactNode }) {
+    const { width, height, left, top } = useDrawingArea();
+    return (
+      <StyledText x={left + width / 2} y={top + height / 2}>
+        {/* <tspan fontSize="2rem" fontWeight="bold">
               124
             </tspan>
             <tspan
@@ -38,61 +37,58 @@ const SummeryWidget = () => {
             >
               Cameras
             </tspan> */}
-            {children}
-          </StyledText>
-        );
-      }
-
+        {children}
+      </StyledText>
+    );
+  }
 
   return (
-    <div className="flex items-center border border-gray-300 rounded-md hover:shadow-xl w-full">
-        {/* pie chart contaier */}
-    <div className="">
-    <StyledEngineProvider injectFirst>
-    {/* StyledEngineProvider is used to override the mui provided styles */}
-    <Box className="w-48 md:w-full " sx={{ padding: 0, margin: 0 }}>
-      <PieChart
-        series={[
-          {
-            data,
-            innerRadius: 35,
-            outerRadius: 75,
-          },
-        ]}
-        colors={colors}
-        legend={{ hidden: true }}
-        {...size}
-      >
-        <PieCenterLabel>
-    {/* Content to be displayed in the center */}
-    <tspan fontSize="1.5rem"></tspan>
-  </PieCenterLabel>
-      </PieChart>
-    </Box>
-  </StyledEngineProvider>
-    </div>
-    {/* summery text container */}
-    <div className=" text-gray-600 pr-2  space-y-2 ">
-        <h3 className="text-xl text-gray-900 font-bold">This Month</h3>
+    <div className="flex items-center justify-between border border-gray-300 rounded-md hover:shadow-xl w-full lg:w-4/12">
+      {/* pie chart contaier */}
+      <div className="w-32">
+        <StyledEngineProvider injectFirst>
+          {/* StyledEngineProvider is used to override the mui provided styles */}
+          <Box className="md:w-full " sx={{ padding: 0, margin: 0 }}>
+            <PieChart
+              series={[
+                {
+                  data,
+                  innerRadius: 30,
+                  outerRadius: 70,
+                },
+              ]}
+              colors={colors}
+              legend={{ hidden: true }}
+              {...size}
+            >
+              <PieCenterLabel>
+                {/* Content to be displayed in the center */}
+                <tspan fontSize="1.5rem"></tspan>
+              </PieCenterLabel>
+            </PieChart>
+          </Box>
+        </StyledEngineProvider>
+      </div>
+      {/* summery text container */}
+      <div className=" text-gray-600 pr-1 md:pr-8 lg:px-2 w-full ">
+        <h3 className="text-xl text-gray-900 font-bold mb-2">This Month</h3>
         <h4 className="flex justify-between">
- <span className="font-semibold text-gray-600">Income: </span>
-  <span className="text-green-500 text-right">৳ -00.00</span>
-</h4>
+          <span className="font-semibold text-gray-600">Income: </span>
+          <span className="text-green-500 text-right">৳ 00.00</span>
+        </h4>
         <h4 className="flex justify-between">
- <span className="font-semibold text-gray-600"> Expenses:  </span>
-  <span className="text-red-500 text-right ml-0 md:ml-2">৳ -00.00</span>
-</h4>
-        <Divider/>
+          <span className="font-semibold text-gray-600"> Expenses: </span>
+          <span className="text-red-500 text-right ml-0 md:ml-2">৳00.00</span>
+        </h4>
+        <Divider />
 
-        <h4 className="flex justify-between">
- <span className="font-semibold text-gray-600">Remaing: </span>
-  <span className="text-green-500 text-right">৳ -00.00</span>
-</h4>
-
-
+        <h4 className="flex justify-between mt-1">
+          <span className="font-semibold text-gray-600 ">Remaing: </span>
+          <span className="text-green-500 text-right">৳ 00.00</span>
+        </h4>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default SummeryWidget
+export default SummeryWidget;
